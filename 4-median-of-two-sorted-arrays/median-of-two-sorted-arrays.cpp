87bec2b3f4c87ebb34b0151b6bf09980abe63ca6
rohan-0295 <itsrohanshaw@gmail.com>
2025-03-30 23:50:53 +0530

    Time: 0 ms (100.00%) | Memory: 95.1 MB (63.52%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
@@ -4,42 +4,32 @@ public:
         vector<int> ans{};
         int m = nums1.size();
         int n = nums2.size();
-        int i = 0;
-        int j = 0;
+        if(n<m) return findMedianSortedArrays(nums2,nums1);
+        int low = 0;
+        int  high = m;
+        int left = (m+n+1)/2;
+        int s = n+m;
+        while(low<=high){
+            int mid1 =  low + (high - low) / 2;
 
-        // Merging two sorted arrays
-        while (i < m && j < n) {
-            if (nums1[i] > nums2[j]) {
-                ans.push_back(nums2[j]);
-                j++;
-            } else {
-                ans.push_back(nums1[i]);
-                i++;
+            int mid2 = left - mid1;
+            int l1 = INT_MIN;
+            int l2 = INT_MIN;
+            int r1 = INT_MAX;
+            int r2 = INT_MAX;
+            if(mid1<m)r1 = nums1[mid1];
+            if(mid2<n)r2 = nums2[mid2];
+            if(mid1 - 1>=0) l1 = nums1[mid1-1];
+            if(mid2 - 1>=0) l2 = nums2[mid2-1];
+            if(l1<=r2 && l2<=r1){
+                if(s%2==1)return max(l1,l2);
+                return ((double)max(l1,l2) + min(r1,r2))/2.0;
             }
+            else if(l1>r2)high = mid1 - 1;
+            else 
+            low = mid1+1;
         }
-
-        // Adding remaining elements from nums2
-        while (j < n) {
-            ans.push_back(nums2[j]);
-            j++;
-        }
-
-        // Adding remaining elements from nums1
-        while (i < m) {
-            ans.push_back(nums1[i]);
-            i++;
-        }
-
-        double median;
-        int size = ans.size();
-
-        // Calculating median
-        if (size % 2 == 1) {  // Odd size
-            median = ans[size / 2];
-        } else {  // Even size
-            median = (ans[size / 2] + ans[(size / 2) - 1]) / 2.0;
-        }
-
-        return median;
+        return 0;
+        
     }
 };
